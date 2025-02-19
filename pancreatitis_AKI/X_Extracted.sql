5dc353cfab5baf89a23075b8c47101d6a85403d1
seri <your.email@example.com>
2025-02-19 00:35:57 -0800

    存档，ap_aki的X,lynn的代码内存不够，明天看看bigquery行不行，再看看能不能属性拆开跑

---
+++ pancreatitis_AKI/X_Extracted.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:883df37b5ac586724e8e775da4bae3f7d7bed7dc182ad905ee213be7165e6148
+size 4106
---
+++ pancreatitis_AKI/final_cohort_from_lynn.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8141d2e45f0baa0161f0f4912cef82af6d2a9092378d90ffddcf8b96f99a55c4
+size 1522
